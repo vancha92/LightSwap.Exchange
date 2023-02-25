64c1394a43d686684e71b1470f8d80d469e0da21
Vancha <fabian.mikita@gmail.com>
2023-02-25 15:39:45 +0100

    new

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+// packages
+import React from "react";
+
+// pages and components
+
+// styles
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,12 @@
+// packages
+import React from "react";
+
+// pages and components
+
+// styles
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
